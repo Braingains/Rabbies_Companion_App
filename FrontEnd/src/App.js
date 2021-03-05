e076614c7918a7357ec88e35ae6199a4cf47dfb5
Ewan Brain <ewan.brain@gmail.com>
2021-03-05 15:53:54 +0000

    default UI removed, click event functioning, clicks saved to state

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {GoogleMap, useLoadScript, Marker, InfoWindow } from "@react-google-maps/api";
-// import { formatRelative } from "date-fns";
+import { formatRelative } from "date-fns";
 
 // import "@reach/combobox/styles.css"
 
@@ -16,11 +16,19 @@ const center = {
   lng: -3.1883
 }
 
+const options = {
+  // styles:  
+  //custom map designs at snazzymaps - save in mapStyles.js
+  disableDefaultUI: true,
+  zoomControl: true,
+} 
+
 export default function App() {
   const {isLoaded, loadError} = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_API_KEY,
     libraries,
   });
+  const [markers, setMarkers] = React.useState([]);
 
   if (loadError) return "Error loading maps"
   if (!isLoaded) return "Loading..."
@@ -28,10 +36,26 @@ export default function App() {
   return (
   
   <div>
-    <GoogleMap mapContainerStyle={mapContainerStyle} 
+    <h1>
+    <span role="img" aria-label="Saltire">🏴󠁧󠁢󠁳󠁣󠁴󠁿 </span>
+    Rabbies
+    <span role="img" aria-label="bus"> 🚎</span>
+    </h1>
+
+    <GoogleMap 
+    mapContainerStyle={mapContainerStyle} 
     zoom={8} 
     center={center}
     options={options}
+    onClick={(event) => {
+    setMarkers(current => [...current, {
+    //spreads in the current markers and adds in the new one
+    lat: event.latLng.lat(),
+    lng: event.latLng.lng(),
+    time: new Date()
+    }])
+
+  }}
     ></GoogleMap> 
   </div>
 
