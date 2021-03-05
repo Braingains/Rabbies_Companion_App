fd9ce3a3ea26f14143d3593bf0c8c33ba586b0cf
Ewan Brain <ewan.brain@gmail.com>
2021-03-05 15:19:12 +0000

    google maps api loaded

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,14 +1,42 @@
-import React from "react";
-import Map from "./components/Map"
+import React from 'react';
+import {GoogleMap, useLoadScript, Marker, InfoWindow } from "@react-google-maps/api";
+// import { formatRelative } from "date-fns";
 
+// import "@reach/combobox/styles.css"
 
+const libraries = ["places"];
+const mapContainerStyle = {
+  width: '100vw',
+  height: '100vh',
+};
 
+//centers on Edinburgh by default
+const center = {
+  lat: 55.9533,
+  lng: -3.1883
+}
 
 export default function App() {
+  const {isLoaded, loadError} = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_API_KEY,
+    libraries,
+  });
+
+  if (loadError) return "Error loading maps"
+  if (!isLoaded) return "Loading..."
+
   return (
-    <>
-    <Map />
-    </>
-  )
-    
-};
+  
+  <div>
+    <GoogleMap mapContainerStyle={mapContainerStyle} 
+    zoom={8} 
+    center={center}
+    options={options}
+    ></GoogleMap> 
+  </div>
+
+  );
+}
+
+
+
--- FrontEnd/src/components/map.js
+++ FrontEnd/src/components/map.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import {GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
+// import React from 'react';
+// import {GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
 
 
-function Map() {
-    return  (
-        <GoogleMap defaultZoom={10} 
-        defaultCenter={{ lat: 55.9533, lng: 3.1883 }}
-        />
-    );
-}
+// function Map() {
+//     return  (
+//         <GoogleMap defaultZoom={10} 
+//         defaultCenter={{ lat: 55.9533, lng: 3.1883 }}
+//         />
+//     );
+// }
 
-const WrappedMap = withScriptjs()
+// const WrappedMap = withScriptjs(withGoogleMap(Map));
 
-export default Map;
+// export default function Map() {
+//     return <>
+
+//         </>
+// }
