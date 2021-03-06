81c98016e29240192ebecfd6a783d761c5dd25b5
Ewan Brain <ewan.brain@gmail.com>
2021-03-06 16:29:21 +0000

    helpers.js and pinContainer.js created

---
+++ FrontEnd/src/containers/PinContainer.js
@@ -0,0 +1,62 @@
+import { getAllByAltText } from '@testing-library/dom';
+import React, { useEffect } from 'react';
+
+const PinContainer = () => {
+
+    const [pins, setPins] = useState([]);
+
+    useEffect(() => {
+        getAllData();
+    }, [pins]);
+
+    const getAllData = () => {
+        console.log("Loading...");
+        const request = new Request();
+        const pinPromise = request.get("/pins")
+
+        // Promise.all might need later
+    } 
+
+    const findPinById = (id) => {
+        console.log(id, pins);
+        return pins.find((pins) => {
+            return pin.id === parseInt(id);
+        });
+      }
+    
+    const handleDelete = (id) =>  {
+        const request = new Request();
+          const url = "/pins/" + id
+          request.delete(url)
+            .then(() => window.location = "/pins")
+        }
+    
+    const handlePost = (pin) => {
+        const request = new Request();
+        request.post("/pins", pin)
+           .then(() => window.location = '/pins')
+      }
+    
+    const handleUpdate = (pin) => {
+        const request = new Request();
+        request.patch('/pins/' + pin.id, pin).then(() => {
+            window.location = '/pins/' + pin.id
+        })
+      }
+
+    if (!{pins}) {
+        return null
+    }
+
+    return (
+        <>
+            <h1>Pin Container</h1>
+        </>
+
+
+
+
+    )
+}
+
+export default PinContainer;
