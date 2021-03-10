fcefd63210c91c365101956bd51f4efe2ad0e225
Ewan Brain <ewan.brain@gmail.com>
2021-03-10 13:51:34 +0000

    can add marker with category

--- FrontEnd/tourcompanion/src/components/MarkerForm.js
+++ FrontEnd/tourcompanion/src/components/MarkerForm.js
@@ -39,9 +39,19 @@ const MarkerForm = ({pin, onCreate, onUpdate}) => {
     return(
         <>
         <h1>this is the marker form</h1>
-        <form onSubmit={handleSubmit}>
+        <form id="MarkerForm" onSubmit={handleSubmit}>
         <input type="text" placeholder="Name" name="name" onChange={handleChange} value={statePin.name} />
-        <input type="text" placeholder="Category" name="category" onChange={handleChange} value={statePin.categoryType} />
+        {/* <input type="select" placeholder="Category" name="category" onChange={handleChange} value={statePin.category} /> */}
+
+        <label for="Category">Marker Category:</label>
+        <select id="Categories" name="category" form="MarkerForm" onChange={handleChange} value={statePin.category}>
+        <option value="Attraction">Attraction</option>
+        <option value="Photo Op">Photo Op</option>
+        <option value="Toilet">Toilet</option>
+        <option value="Food">Food</option>
+        <option value="Parking">Parking</option>
+        </select>
+
         <input type="text" placeholder="Notes" name="notes" onChange={handleChange} value={statePin.notes} />
         <input type="text" placeholder="User" name="user" onChange={handleChange} value={statePin.user} />
         <input type="decimal" placeholder="Latitude" name="lat" onChange={handleChange} value={statePin.lat}/>
