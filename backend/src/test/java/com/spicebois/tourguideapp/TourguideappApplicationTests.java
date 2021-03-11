59a3e5aa5b5535c01b7be7b7eeceaffb8d926ffd
m6ttw <matthewrhyswilliams@live.com>
2021-03-11 18:30:03 +0000

    final commit hopefully

---
+++ backend/src/test/java/com/spicebois/tourguideapp/TourguideappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spicebois.tourguideapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourguideappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
